b4055ac951c56ee600c461feb80064f2a5a42317
GUEVARA0331 <jdguevara01@misena.edu.co>
2023-02-05 21:51:24 -0500

    Grids

---
+++ resources/views/grids.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.model')
+
+@section('title', 'Grids')
+
+@section('content')
+
+    <div class="container">
+        <div class="grid grid-cols-4 gap-4">
+            <div class="bg-blue-200 md:col-span-3 lg:col-span-4">A</div>
+            <div class="bg-blue-300">B</div>
+            <div class="bg-blue-400">C</div>
+            <div class="bg-blue-500">D</div>
+            <div class="bg-blue-600">E</div>
+            <div class="bg-blue-700">F</div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="grid grid-flow-col grid-rows-3 grid-cols-4">
+            <div class="bg-blue-100">1</div>
+            <div class="bg-blue-200">2</div>
+            <div class="bg-blue-300">3</div>
+            <div class="bg-blue-400">4</div>
+            <div class="bg-blue-500">5</div>
+            <div class="bg-blue-600">6</div>
+            <div class="bg-blue-700">7</div>
+            <div class="bg-blue-800">8</div>
+            <div class="bg-blue-900">9</div>
+        </div>
+    </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/grids', function () {
+    return view('grids');
+})->name('tailwind.grids');
